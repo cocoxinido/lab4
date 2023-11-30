c41b59634a59108a55a6164b7b570f2b14101a3f
cocoxinido <nikita2004bulahov@gmail.com>
2023-11-30 23:54:35 +0700

    Добавьте файлы проекта.

---
+++ lab4BulahovVasilyev/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4BulahovVasilyev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какая операция вам нужна, введите цифру?" + Environment.NewLine + "1. Сложение " + Environment.NewLine + "2. Вычитание " + Environment.NewLine + "3. Умножение " + Environment.NewLine + "4. Деление ");
+            string k = Console.ReadLine();
+            Console.WriteLine("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
